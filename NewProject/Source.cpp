63e76f15a2508c689c9887c5d6585bc1422e4bf9
anvar123 <anvar123@gmail.com>
2023-05-07 09:29:50 +0400

    My commit

---
+++ NewProject/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	cout << "Enter first number : ";
+	int num1 = 0;
+	cin >> num1;
+	cout << "Enter second number : ";
+	int num2 = 0;
+	cin >> num2;
+	cout << "Sum : " << num1 + num2 << endl;
+}
